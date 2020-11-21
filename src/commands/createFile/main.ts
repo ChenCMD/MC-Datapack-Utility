34b87a4a5c4c1921cf43d65d37945c40ecd9c766
ChenCMD <scnmed.g@gmail.com>
2020-11-21 12:54:29 +0900

    :recycle: fix createFile not error catch

--- src/commands/createFile/main.ts
+++ src/commands/createFile/main.ts
@@ -8,7 +8,8 @@ import { getFileTemplate } from './utils';
 import { TextEncoder } from 'util';
 import { FileType, getFileType } from '../../types/FileTypes';
 import { resolveVars, VariableContainer } from '../../types/VariableContainer';
-import { config } from '../../extension';
+import { codeConsole, config } from '../../extension';
+import { UserCancelledError } from '../../types/Error';
 
 export async function createFile(uri: Uri): Promise<void> {
     // Datapack内か確認
@@ -34,52 +35,55 @@ export async function createFile(uri: Uri): Promise<void> {
     const fileExtname = fileType === 'function' ? 'mcfunction' : 'json';
 
     // ファイル名入力
-    const fileName = await listenInput(locale('create-file.file-name', fileType), async v => {
-        const invalidChar = v.match(/[^a-z0-9./_-]/g);
-        if (invalidChar)
-            return locale('error.unexpected-character', invalidChar.join(', '));
-        if (await pathAccessible(path.join(uri.fsPath, `${v}.${fileExtname}`)))
-            return locale('create-file.already-exists', `${v}.${fileExtname}`);
-        return undefined;
-    });
-    if (fileName === undefined) return;
+    try {
+        const fileName = await listenInput(locale('create-file.file-name', fileType), async v => {
+            const invalidChar = v.match(/[^a-z0-9./_-]/g);
+            if (invalidChar)
+                return locale('error.unexpected-character', invalidChar.join(', '));
+            if (await pathAccessible(path.join(uri.fsPath, `${v}.${fileExtname}`)))
+                return locale('create-file.already-exists', `${v}.${fileExtname}`);
+            return undefined;
+        });
 
-    // リソースパスの生成とファイルテンプレートの取得
-    const filePath = path.join(uri.fsPath, `${fileName}.${fileExtname}`);
+        // リソースパスの生成とファイルテンプレートの取得
+        const filePath = path.join(uri.fsPath, `${fileName}.${fileExtname}`);
 
-    const variableContainer: VariableContainer = {
-        datapackName: path.basename(datapackRoot),
-        namespace: getNamespace(filePath, datapackRoot),
+        const variableContainer: VariableContainer = {
+            datapackName: path.basename(datapackRoot),
+            namespace: getNamespace(filePath, datapackRoot),
 
-        fileResourcePath: getResourcePath(filePath, datapackRoot, fileType),
-        fileName,
-        fileType,
-        fileExtname,
+            fileResourcePath: getResourcePath(filePath, datapackRoot, fileType),
+            fileName,
+            fileType,
+            fileExtname,
 
-        date: getDate(config.dateFormat),
-        cursor: ''
-    };
+            date: getDate(config.dateFormat),
+            cursor: ''
+        };
 
 
-    const openFilePath = window.activeTextEditor?.document.uri.fsPath;
-    if (openFilePath) {
-        const nowOpenFileType = getFileType(path.dirname(openFilePath), datapackRoot) ?? '';
-        variableContainer['nowOpenFileType'] = nowOpenFileType;
-        variableContainer['nowOpenFileResourcePath'] = nowOpenFileType !== '' ? getResourcePath(openFilePath, datapackRoot, nowOpenFileType as FileType) : '';
-        variableContainer['nowOpenFileName'] = openFilePath.match(/([^/\\]*(?=\.(?!.*\.))|(?<=^|(?:\/|\\))[^./\\]*$)/)?.shift() ?? '';
-        variableContainer['nowOpenFileExtname'] = openFilePath.match(/(?<=\.)[^./\\]*?$/)?.shift() ?? '';
+        const openFilePath = window.activeTextEditor?.document.uri.fsPath;
+        if (openFilePath) {
+            const nowOpenFileType = getFileType(path.dirname(openFilePath), datapackRoot) ?? '';
+            variableContainer['nowOpenFileType'] = nowOpenFileType;
+            variableContainer['nowOpenFileResourcePath'] = nowOpenFileType !== '' ? getResourcePath(openFilePath, datapackRoot, nowOpenFileType as FileType) : '';
+            variableContainer['nowOpenFileName'] = openFilePath.match(/([^/\\]*(?=\.(?!.*\.))|(?<=^|(?:\/|\\))[^./\\]*$)/)?.shift() ?? '';
+            variableContainer['nowOpenFileExtname'] = openFilePath.match(/(?<=\.)[^./\\]*?$/)?.shift() ?? '';
+        }
+
+        const fileTemplate = getFileTemplate(config.createFile.fileTemplate, fileType);
+        let selection = undefined;
+        fileTemplate.forEach((v, i) => {
+            const res = v.search(/%cursor%/i);
+            if (res !== -1) selection = new Range(i, res, i, res);
+        });
+
+        // 生成
+        await create(filePath, new TextEncoder().encode(resolveVars(fileTemplate.join('\r\n'), variableContainer)));
+        // ファイルを開く
+        await window.showTextDocument(Uri.file(filePath), { selection });
+    } catch (error) {
+        if (error instanceof UserCancelledError) return;
+        codeConsole.appendLine(error.toString());
     }
-
-    const fileTemplate = getFileTemplate(config.createFile.fileTemplate, fileType);
-    let selection = undefined;
-    fileTemplate.forEach((v, i) => {
-        const res = v.search(/%cursor%/i);
-        if (res !== -1)
-            selection = new Range(i, res, i, res);
-    });
-
-    // 生成
-    await create(filePath, new TextEncoder().encode(resolveVars(fileTemplate.join('\r\n'), variableContainer)));
-    // ファイルを開く
-    await window.showTextDocument(Uri.file(filePath), { selection });
 }
