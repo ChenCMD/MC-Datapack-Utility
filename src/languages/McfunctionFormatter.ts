fbbd57eb0e948fc3279d9018ee2a81c689457602
t5ugu <tsugu531@gmail.com>
2021-03-30 17:59:31 +0900

    遠隔にておこなった変更が心もとないので、commitし直し

--- src/languages/McfunctionFormatter.ts
+++ src/languages/McfunctionFormatter.ts
@@ -61,6 +61,7 @@ export class McfunctionFormatter implements DocumentFormattingEditProvider {
                 next(range, line, indentElement);
                 continue;
             }
+
             // コメントについての処理
             switch (line.slice(docText.cursor - lineStart, line.indexOf(' '))) {
                 case '': // 「# ～」や「## ～」の場合
@@ -83,6 +84,7 @@ export class McfunctionFormatter implements DocumentFormattingEditProvider {
                 case '>':
                     depth.clear();
                     depth.addLast(numSigns);
+
                     indentElement.newLineSign = lineCount === 0 ? '' : '\n';
                     indentElement.newLineSign += lastLineType === 'command' ? '\n' : '';
                     indentElement.indents = 0;
@@ -98,6 +100,7 @@ export class McfunctionFormatter implements DocumentFormattingEditProvider {
     private async insertProtocol(document: TextDocument): Promise<TextEdit[]> {
         if (!config.mcfFormatter.doInsertIMPDocument)
             return [];
+
         const rootPath = await getDatapackRoot(document.fileName);
 
         if (rootPath) {
