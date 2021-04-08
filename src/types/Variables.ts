22175829db4a4b2067cca0dcce25aa06d63d5693
ChenCMD <scnmed.g@gmail.com>
2021-04-08 14:56:56 +0900

    :construction: Refactor Context interface and resolveVars

---
+++ src/types/Variables.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { isObjectArray, isStringArray } from '../utils/typeGuards';
+import { isJsonObject, JsonObject, JsonValue } from './JsonObject';
+
+export type Variables = { [key: string]: string };
+
+export function resolveVars<T extends string | string[] | JsonObject | JsonObject[]>(obj: T, vars: Variables):
+    T extends string ? string
+    : T extends string[] ? string[]
+    : T extends JsonObject ? JsonObject
+    : T extends JsonObject[] ? JsonObject[]
+    : T {
+    const resolve = (str: string) => str.replace(/%.+?%/g, match => {
+        const key = match.slice(1, -1);
+        return Object.keys(vars).includes(key) ? vars[key] : match;
+    });
+    const walkObj = (_obj: JsonObject): JsonObject => {
+        const process = (elem: JsonValue): JsonValue => {
+            if (Array.isArray(elem)) return elem.map(v => process(v)); // JsonValue[]
+            if (typeof elem === 'object') return walkObj(elem as JsonObject); // JsonObject
+            if (typeof elem === 'string') return resolve(elem); // string
+            return elem;
+        };
+        for (const key of Object.keys(_obj)) _obj[key] = process(_obj[key]);
+        return _obj;
+    };
+
+    if (Array.isArray(obj)) {
+        if (isStringArray(obj)) return obj.map(resolve) as any; // string[]
+        if (isObjectArray(obj)) return obj.map(walkObj) as any; // JsonObject[]
+        return obj as any;
+    } else {
+        if (typeof obj === 'string') return resolve(obj) as any; // string
+        if (isJsonObject(obj)) return walkObj(obj) as any; // JsonObject
+        return obj as any;
+    }
+}
