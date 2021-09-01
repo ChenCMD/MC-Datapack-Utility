9077afc80e74ad44a75ef233fa4cbfccdecd73c5
ChenCMD <scnmed.g@gmail.com>
2021-09-02 03:15:17 +0900

    :adhesive_bandage: fix locale involved core system

--- src/commands/createDatapackTemplate/nodes/index.ts
+++ src/commands/createDatapackTemplate/nodes/index.ts
@@ -1,4 +1,3 @@
-import { locale } from '../../../locales';
 import { AddTemplateGenNode } from './AddTemplateGenNode';
 import { CreateTemplateGenNode } from './CreateTemplateGenNode';
 
@@ -11,7 +10,7 @@ export type GenNodes =
 
 export function getGenTypeMap(): Map<string, GenNodes> {
     const res = new Map<string, GenNodes>();
-    res.set(locale('create-datapack-template.add'), AddTemplateGenNode);
-    res.set(locale('create-datapack-template.create'), CreateTemplateGenNode);
+    res.set('create-datapack-template.add', AddTemplateGenNode);
+    res.set('create-datapack-template.create', CreateTemplateGenNode);
     return res;
 }
